992a066c94b8f666aed9efaa35d394526252d3f2
Jeferson <jeferson_jb@live.com>
2023-07-04 17:06:54 -0300

    fix: Header component styling

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,39 +1,40 @@
 import styled from "styled-components";
 
 export const HeaderStyled = styled.header`
-   
-    background: transparent;
+  background: transparent;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 80px;
+  > div {
     display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    padding: 20px;
-    >div{
-        display: flex;
-        gap: 20px;
-    
-        
-        a{
-            text-decoration: none;
-            font-family: Poppins;
-            font-family: Poppins;
-            font-size: 17px;
-            font-weight: 700;
-            line-height: 26px;
-            width: max-content;
-            padding-inline:20px;
-            background: var(--color-yellow);
-            height: 40px;
-            border-radius: 255px;
-            text-align: center;
-            display: flex;
-            align-items: center;
-            color: var(--color-black);
-        }
-        a.register{
-            background: transparent;
-            color: var(--color-white);
+    gap: 20px;
 
-        }
+    a {
+      text-decoration: none;
+      font-family: Poppins;
+      font-size: 17px;
+      font-weight: 700;
+      line-height: 26px;
+      width: max-content;
+      padding-inline: 15px;
+      background: var(--color-yellow);
+      height: 40px;
+      border-radius: 255px;
+      text-align: center;
+      display: flex;
+      align-items: center;
+      color: var(--color-black);
     }
-`
+    a.register {
+      background: transparent;
+      color: var(--color-white);
+      display: none;
+      @media (min-width: 600px) {
+        display: flex;
+      }
+    }
+  }
+`;
