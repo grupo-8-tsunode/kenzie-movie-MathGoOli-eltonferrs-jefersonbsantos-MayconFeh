71a5e65807acb5202cb25d1d63351439a9141715
Jeferson <jeferson_jb@live.com>
2023-07-04 16:08:28 -0300

    feat: register page styling

---
+++ src/pages/Register/style.ts
@@ -0,0 +1,21 @@
+import { styled } from "styled-components";
+
+export const RegisterStyled = styled.div`
+  a {
+    font-family: "Poppins";
+    color: var(--color-black);
+    text-decoration: none;
+    padding-inline: 15px;
+  }
+
+  .nav__container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .signUp__button {
+    color: var(--color-white);
+    font-weight: 700;
+  }
+`;
