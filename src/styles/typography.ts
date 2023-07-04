9663c0bbe350ef9564769a6cf54a29f95c80d8c2
Maycon Felipe <mfbm87@gmail.com>
2023-07-04 11:30:38 -0300

    add-typography

---
+++ src/styles/typography.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const H1Styled = styled.h1`
+  font-family: Poppins;
+  font-size: 2.75rem;
+  font-weight: 700;
+  line-height: 4.125rem;
+
+`
+export const H3Styled = styled.h3`
+  font-family: Poppins;
+  font-size: 1.625rem;
+  font-weight: 700;
+  line-height: 2.4375rem;
+
+`
+
+export const PStyled = styled.p`
+
+  font-family: Inter;
+  font-size: 1.125rem;
+  font-weight: 400;
+  line-height: 2.25rem;
+
+`
