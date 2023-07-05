d5f2428a05622be9f464bcc222acf309dd416f24
Jeferson <jeferson_jb@live.com>
2023-07-05 18:58:33 -0300

    feat: review avaliation

--- src/providers/@types.ts
+++ src/providers/@types.ts
@@ -1,60 +1,62 @@
 import { TypeResgisterFormValue } from "../pages/Register/registerSchema";
 
-export interface IUser{
-    email: string;
-    name: string;
-    id: number;
+export interface IUser {
+  email: string;
+  name: string;
+  id: number;
 }
 
-export interface ILogin{
-    accessToken: string;
-    user: IUser;
+export interface ILogin {
+  accessToken: string;
+  user: IUser;
 }
 
-export interface IChildren{
-    children: React.ReactNode
+export interface IChildren {
+  children: React.ReactNode;
 }
 
-export interface ISubmit{
-    email: string;
-    password: string;
+export interface ISubmit {
+  email: string;
+  password: string;
 }
 
-export interface IUserContext{
-    isLoading: boolean;
-    user: IUser | null;
-    loadUser: () => Promise<void>;
-    userLoginSubmit: (formData: ISubmit) => Promise<void>;
-    userLogoff: () => void;
-    userAddReview: (review: IReview) => Promise<void>;
-    userEditReview: (review: IReview, movieId: string) => Promise<void>;
-    userDeleteReview: (movieId: string) => Promise<void>;
-    registerNewuser: (data: TypeResgisterFormValue) => Promise<void>;
-  
-  }
-
-
-export interface IReview{
-    id: number;
-    movieId: number;
-    userId: number;
-    score: number;
-    description: string;
+export interface IUserContext {
+  isLoading: boolean;
+  user: IUser | null;
+  loadUser: () => Promise<void>;
+  userLoginSubmit: (formData: ISubmit) => Promise<void>;
+  userLogoff: () => void;
+  userAddReview: (review: IReview) => Promise<void>;
+  userEditReview: (review: IReview, movieId: string) => Promise<void>;
+  userDeleteReview: (movieId: string) => Promise<void>;
+  registerNewuser: (data: TypeResgisterFormValue) => Promise<void>;
 }
 
-export interface IMovie{
-    id: number;
-    name: string;
-    type: string;
-    duration: number;
-    synopsis: string;
-    image: string;
-    reviews: IReview[];
+export interface IReview {
+  length: any;
+  id: number;
+  movieId: number;
+  userId: number;
+  score: number;
+  description: string;
 }
 
-export interface IMovieContext{
-    movies: IMovie[];
-    getMovies: () => Promise<void>;
-    getMovie: (id: number) => Promise<IMovie> | Promise<unknown>;
-    getReview: (idMovie: number, idUser: number) => Promise<IReview> | Promise<unknown>;
+export interface IMovie {
+  id: number;
+  name: string;
+  type: string;
+  duration: number;
+  synopsis: string;
+  image: string;
+  reviews: IReview[];
+}
+
+export interface IMovieContext {
+  movies: IMovie[];
+  getMovies: () => Promise<void>;
+  getMovie: (id: number) => Promise<IMovie> | Promise<unknown>;
+  getReview: (
+    idMovie: number,
+    idUser: number
+  ) => Promise<IReview> | Promise<unknown>;
 }
