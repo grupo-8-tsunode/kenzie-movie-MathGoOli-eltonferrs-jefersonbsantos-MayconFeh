b9b2b6547234118de54129a1b3ad27a94cce363f
Jeferson <jeferson_jb@live.com>
2023-07-05 11:11:56 -0300

    feat: mobile Login page styling

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -5,35 +5,50 @@ import { UserContext } from "../../providers/UserContext";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { TypeLoginFormValue, loginSchema } from "./loginSchema";
+import { LoginStyled } from "./styles";
+import { H1Styled } from "../../styles/typography";
+import { Input } from "../../components/Inputs/Index";
 
 export const Login = () => {
- 
-  const{ userLoginSubmit  } = useContext(UserContext)
+  const { userLoginSubmit } = useContext(UserContext);
 
-  const {register,handleSubmit,reset, formState: {errors}} = useForm<TypeLoginFormValue>({resolver: zodResolver(loginSchema)})
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<TypeLoginFormValue>({ resolver: zodResolver(loginSchema) });
 
-  const submit:SubmitHandler<TypeLoginFormValue> =(data) =>{
-    userLoginSubmit(data)
-    reset()
-  }
- return(
-    <>
-       <Header/>
-       <div>
-          <form onSubmit={handleSubmit(submit)} >
-            <h2>Login</h2>
-            <input type="email" placeholder="Email"
-            {...register("email")}/>
-            {errors.email? <p>{errors.email.message}</p>:null}
-            <input type="password" placeholder="Senha"
-            {...register("password")}/>
-            {errors.password? <p>{errors.password.message}</p>:null}
-            <button type="submit" >Entrar</button>
-          </form>
+  const submit: SubmitHandler<TypeLoginFormValue> = (data) => {
+    userLoginSubmit(data);
+    reset();
+  };
+  return (
+    <LoginStyled>
+      <Header />
+      <div>
+        <form onSubmit={handleSubmit(submit)}>
+          <H1Styled>Login</H1Styled>
+          <Input type="email" placeholder="Email" {...register("email")} />
+          {errors.email ? <p>{errors.email.message}</p> : null}
+          <Input
+            type="password"
+            placeholder="Senha"
+            {...register("password")}
+          />
+          {errors.password ? <p>{errors.password.message}</p> : null}
+          <button className="login__button" type="submit">
+            Entrar
+          </button>
+        </form>
+        <div className="signUp__div">
           <p>ou</p>
-          <Link to={"/register"}>Cadastre-se</Link>
+          <Link to={"/register"} className="signUp__button">
+            Cadastre-se
+          </Link>
         </div>
-        {/*footer */}
-    </>
-  ) 
+      </div>
+      {/*footer */}
+    </LoginStyled>
+  );
 };
