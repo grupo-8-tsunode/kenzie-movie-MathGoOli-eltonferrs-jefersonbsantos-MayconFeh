74534ebd4e82662236b90498edced9358a742ec0
Maycon Felipe <mfbm87@gmail.com>
2023-07-06 20:27:40 -0300

    feat: add-loading-all-page

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -56,4 +56,51 @@ export const GlobalStyle = createGlobalStyle`
     p {
         font-family: 'Inter', sans-serif
     }
+
+    @keyframes spin {
+  to {
+    transform: rotate(360deg);
+  }
+}
+.loading{
+  width: 60px;
+  height: 60px;
+  border-radius:50%;
+  animation: spin 1.2s linear infinite;
+ 
+  &::before,
+  &::after{
+  content: "";
+  position: absolute;
+  border-radius: inherit;
+  }
+  &::before{
+    width: 100%;
+    height: 100%;
+   background-image: 
+   linear-gradient(0deg,
+    var(--color-yellow) 0%,
+    var(--color-black) 100%
+    );
+  }
+    &::after{
+      width: 85%;
+    height: 85%;
+    background:var(--color-lightgray-2);
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    }
+  }
+
+  .loadingContainer{
+  display: flex;
+  flex-direction:column;
+  justify-content: center;
+  align-items: center;
+  width : 100% ;
+  height: 100%;
+  position: absolute;
+  
+  }
 `;
